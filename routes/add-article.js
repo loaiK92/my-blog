0613285bc9a1d3f6e2abaa03008a49f0b1b5e9f3
loaikahel1992 <loaikahel723@gmail.com>
2018-08-12 14:06:02 +0200

    change images to base64

--- routes/add-article.js
+++ routes/add-article.js
@@ -10,7 +10,7 @@ Router.use(bodyParser.json());
 
 const articleImageStorage = multer.diskStorage({
   destination: function(req, file, cb) {
-    cb(null, "../images");
+    cb(null, "../public/images");
   },
   filename: function(req, file, cb) {
     cb(
